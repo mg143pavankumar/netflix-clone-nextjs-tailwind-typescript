64e4aede7d2fd14797b3e12d33932c84aa3c4102
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-04-18 01:10:09 +0530

    initial commit

---
+++ components/Banner.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { baseUrl } from '../constants/movie'
+import { Movie } from '../typing'
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+  console.log(movie)
+
+  return (
+    <div>
+      <div className="absolute top-0 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Banner
